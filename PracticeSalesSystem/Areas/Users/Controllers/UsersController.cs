ca580255be550a83cce9366fcfd83d0cace3d8a5
Lucas <lucasabalos@maersoft.com.ar>
2022-12-05 10:32:10 -0300

    Creo mi propia area y vista para registrar usuarios

---
+++ PracticeSalesSystem/Areas/Users/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeSalesSystem.Areas.User.Controllers
+{
+
+    //Cada vez que agregue un controlador dentro de una area(En este caso Area: User )
+    //Tenemos que especificarle en QUE area se encuentra.
+    [Area("Users")]
+    public class UsersController : Controller
+    {
+        public IActionResult Users()
+        {
+            return View();
+        }
+    }
+}
